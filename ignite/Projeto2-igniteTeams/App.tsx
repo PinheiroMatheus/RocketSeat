6e6dbd0929212313ed056c02c9977323c14f007c
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2023-12-09 19:55:29 -0300

    Alterando a fonte do app

--- ignite/Projeto2-igniteTeams/App.tsx
+++ ignite/Projeto2-igniteTeams/App.tsx
@@ -1,12 +1,18 @@
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
 
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   )
 }
