bd9d75ac28c8e515a438a60ccf86f916c2a01798
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2023-12-11 18:55:39 -0300

    Criando e estilizando o botao principal

--- ignite/Projeto2-igniteTeams/src/screens/Groups/index.tsx
+++ ignite/Projeto2-igniteTeams/src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import {
   Container,
@@ -36,6 +37,10 @@ export function Groups() {
             message='Que tal cadastrar a primeira turma?'
           />
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
 
     </Container>
