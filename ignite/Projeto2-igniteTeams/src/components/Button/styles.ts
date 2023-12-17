988525e196c92951da2a856f82adbde15eb6be52
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2023-12-17 20:29:05 -0300

    Corrigindo o font-size do button

--- ignite/Projeto2-igniteTeams/src/components/Button/styles.ts
+++ ignite/Projeto2-igniteTeams/src/components/Button/styles.ts
@@ -22,7 +22,7 @@ export const Container = styled(TouchableOpacity) <Props>`
 `;
 
 export const Title = styled.Text`
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD};
+    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
     font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
     color: ${({ theme }) => theme.COLORS.WHITE}
 `;
