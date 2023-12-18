fd0d0280010db6a574a37fc6ae7fd99dedc5b2c6
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2023-12-17 21:52:55 -0300

    Criando a pagina Players

--- ignite/Projeto2-igniteTeams/App.tsx
+++ ignite/Projeto2-igniteTeams/App.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from "react-native";
-import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
 import { Loading } from "@components/Loading";
 
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -18,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <NewGroup /> : <Loading /> }
+      { fontsLoaded ? <Players /> : <Loading /> }
     </ThemeProvider>
   )
 }
