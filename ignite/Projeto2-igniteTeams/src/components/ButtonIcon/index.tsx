264d21f5e63768b3b757e5b802bb0f300589b8cd
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2023-12-19 21:38:27 -0300

    Deixando o icone dinamico

--- ignite/Projeto2-igniteTeams/src/components/ButtonIcon/index.tsx
+++ ignite/Projeto2-igniteTeams/src/components/ButtonIcon/index.tsx
@@ -1,16 +1,18 @@
 import { TouchableOpacityProps } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons'
 
-import { Container, Icon } from "./styles";
+import { ButtonIconTypeStyleProps, Container, Icon } from "./styles";
 
 type Props = TouchableOpacityProps & {
-
+    icon: keyof typeof MaterialIcons.glyphMap;
+    type?: ButtonIconTypeStyleProps;
 }
 
-export function ButtonIcon({  }: Props) {
+export function ButtonIcon({ icon, type = 'PRIMARY', ...rest }: Props) {
     return (
-        <Container>
+        <Container {...rest}>
             <Icon
-                name='home'
+                name={icon}
                 type='PRIMARY'
             />
         </Container>
