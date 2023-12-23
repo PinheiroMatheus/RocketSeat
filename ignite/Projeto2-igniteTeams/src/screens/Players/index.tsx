5514a330774fc977bcbbcc1eb689194bc8508e0f
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2023-12-23 14:08:43 -0300

    Criando o componente players

--- ignite/Projeto2-igniteTeams/src/screens/Players/index.tsx
+++ ignite/Projeto2-igniteTeams/src/screens/Players/index.tsx
@@ -6,12 +6,15 @@ import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 export function Players() {
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState(['Matheus', 'Teste'])
 
     return (
         <Container>
@@ -51,6 +54,32 @@ export function Players() {
                     {players.length}
                 </NumbersOfPlayers>
             </HeaderList>
+
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <PlayerCard
+                        name={item}
+                        onRemove={() => { }}
+                    />
+                )}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message='Não há pessoas nesse time'
+                    />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    { paddingBottom: 100 },
+                    players.length === 0 && { flex: 1 }
+                ]}
+            />
+
+            <Button
+                title='Remover Turma'
+                type='SECONDARY'
+            />
         </Container>
     )
 }
