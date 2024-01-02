2f8ca3f478edbbb3b11a53ff68594f0c4f75d009
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-01-02 20:31:50 -0300

    Navegando pela aplicação

--- ignite/Projeto2-igniteTeams/src/screens/Groups/index.tsx
+++ ignite/Projeto2-igniteTeams/src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -13,6 +14,12 @@ import {
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -41,7 +48,7 @@ export function Groups() {
 
       <Button 
         title='Criar nova turma'
-        onPress={() => {}}
+        onPress={handleNewGroup}
       />
 
     </Container>
