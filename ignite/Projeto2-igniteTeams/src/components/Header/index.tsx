beca72925b3b0a32a1d49a9075beca94d1ffcf28
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-01-02 20:44:40 -0300

    Implementando o voltar

--- ignite/Projeto2-igniteTeams/src/components/Header/index.tsx
+++ ignite/Projeto2-igniteTeams/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Logo, BackButton, BackIcon } from "./styles"
 
 import logoImg from '@assets/logo.png'
@@ -7,11 +9,17 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props) {
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.navigate('groups')
+    }
+
     return (
         <Container>
             {
                 showBackButton && /* Se showBackButton for verdadeiro */ 
-                <BackButton>
+                <BackButton onPress={handleGoBack}>
                     <BackIcon />
                 </BackButton>
             }
