fef27189c300e16133b0764bb8e64c4b8a7d8f9f
Matheus Pinheiro de Sousa <Matheus@MacBook-de-Matheus-P.local>
2024-01-21 18:33:24 -0300

    Removendo Player do Grupo

--- ignite/Projeto2-igniteTeams/src/screens/Players/index.tsx
+++ ignite/Projeto2-igniteTeams/src/screens/Players/index.tsx
@@ -7,6 +7,7 @@ import { AppError } from '@utils/AppError';
 import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
 import { playerAddByGroup } from '@storage/player/playerAddByGroup';
 import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
+import { playerRemoveByGroup } from '@storage/player/playerRemoveByGroup';
 
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
@@ -69,6 +70,16 @@ export function Players() {
         }
     }
 
+    async function handlePlayerRemove(playerName: string) {
+        try {
+            await playerRemoveByGroup(playerName, group)
+            fetchPlayersByTeam();
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa.');
+        }
+    }
+
     useEffect(() => {
         fetchPlayersByTeam();
     }, [team]);
@@ -124,7 +135,7 @@ export function Players() {
                 renderItem={({ item }) => (
                     <PlayerCard
                         name={item.name}
-                        onRemove={() => { }}
+                        onRemove={() => handlePlayerRemove(item.name)}
                     />
                 )}
                 ListEmptyComponent={() => (
